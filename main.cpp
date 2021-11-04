007db1e399a8ae9b1ef3807105798826b17b4a07
ScienceLuke7 <luke100700@gmail.com>
2021-11-04 17:31:02 -0300

    init commit; boiler plate code

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#define HALT_OPCODE 0x19
+
+void fetchNextInstruction(void);
+void executeInstruction(void);
+
+unsigned char memory[65536];
+unsigned char ACC = 0;
+unsigned char IR = 0;
+unsigned int MAR = 0;
+unsigned int PC = 0;
+
+
+int main(int argc, char* argv[]) {
+
+    while(memory[PC] != HALT_OPCODE) {
+        fetchNextInstruction();
+        executeInstruction();
+    }
+    return 0;
+
+}
